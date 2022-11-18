d322f0ab9bd595303e8d494cfd9eb5d487749ea0
smooti <kobeowens43099@gmail.com>
2022-11-18 10:28:22 -0500

    initial

---
+++ Scripts/Set-NetworkTypeToPrivate.ps1
@@ -0,0 +1,5 @@
+﻿# You cannot enable Windows PowerShell Remoting on network connections that are set to Public
+# Spin through all the network locations and if they are set to Public, set them to Private
+# using the INetwork interface:
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa370750(v=vs.85).aspx
+Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
