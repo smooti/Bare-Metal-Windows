bceca1ada8a98ce8069908891e804bf13edb1b88
smooti <kobeowens43099@gmail.com>
2022-11-18 13:15:40 -0500

    Changing bat to ps1

---
+++ Scripts/Enable-Updates.ps1
@@ -0,0 +1,10 @@
+Stop-Service -Name "wuauserv"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "EnableFeaturedSoftware" -PropertyType DWORD -Value 1
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "IncludeRecommendedUpdates" -PropertyType DWORD -Value 1
+
+echo Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager") > A:\temp.vbs
+echo Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") >> A:\temp.vbs
+
+cscript A:\temp.vbs
+
+Start-Service -Name "wuauserv"
