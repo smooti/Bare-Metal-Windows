daec5730bd2e7a7dd5259e8509ae3e42b8d5e75c
smooti <kobeowens43099@gmail.com>
2022-11-18 13:18:51 -0500

    changing command name

--- Scripts/Enable-Updates.ps1
+++ Scripts/Enable-Updates.ps1
@@ -2,8 +2,9 @@ Stop-Service -Name "wuauserv"
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "EnableFeaturedSoftware" -PropertyType DWORD -Value 1
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" -Name "IncludeRecommendedUpdates" -PropertyType DWORD -Value 1
 
-echo Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager") > A:\temp.vbs
-echo Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") >> A:\temp.vbs
+# Register the Microsoft Update service with Automatic Updates
+Write-Output Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager") > A:\temp.vbs
+Write-Output Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d", 7, "") >> A:\temp.vbs
 
 cscript A:\temp.vbs
 
