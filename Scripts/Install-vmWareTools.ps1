f2636ad6f08880e1edb7e06ad7a0134687d914c5
smooti <kobeowens43099@gmail.com>
2022-11-18 19:14:10 -0500

    init

--- Scripts/Install-vmWareTools.ps1
+++ Scripts/Install-vmWareTools.ps1
@@ -1,96 +1,61 @@
-if (!( Test-Path "C:\Windows\Temp\7z1900-x64.msi")) {
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z1900-x64.msi', 'C:\Windows\Temp\7z1900-x64.msi')
-}
-if (!(Test-Path "C:\Windows\Temp\7z1900-x64.msi")) {
-	Start-Sleep 5; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z1900-x64.msi', 'C:\Windows\Temp\7z1900-x64.msi')
-}
-cmd /c msiexec /qb /i C:\Windows\Temp\7z1900-x64.msi
-
-if ("$env:PACKER_BUILDER_TYPE" -eq "vmware-iso") {
-
-	Write-Output "Using VMware"
-	if (Test-Path "C:\Users\vagrant\windows.iso") {
-		Move-Item -Force C:\Users\vagrant\windows.iso C:\Windows\Temp
-	}
-
-	if (!(Test-Path "C:\Windows\Temp\windows.iso")) {
-		Try {
-			# Disabling the progress bar speeds up IWR https://github.com/PowerShell/PowerShell/issues/2138
-			$ProgressPreference = 'SilentlyContinue'
-			$pageContentLinks = (Invoke-WebRequest('https://softwareupdate.vmware.com/cds/vmw-desktop/ws') -UseBasicParsing).Links | Where-Object { $_.href -Match "[0-9]" } | Select-Object href | ForEach-Object { $_.href.Trim('/') }
-			$versionObject = $pageContentLinks | ForEach-Object { New-Object System.Version ($_) } | Sort-Object -Descending | Select-Object -First 1 -Property:Major, Minor, Build
-			$newestVersion = $versionObject.Major.ToString() + "." + $versionObject.Minor.ToString() + "." + $versionObject.Build.ToString() | Out-String
-			$newestVersion = $newestVersion.TrimEnd("`r?`n")
-
-			$nextURISubdirectoryObject = (Invoke-WebRequest("https://softwareupdate.vmware.com/cds/vmw-desktop/ws/$newestVersion/") -UseBasicParsing).Links | Where-Object { $_.href -Match "[0-9]" } | Select-Object href | Where-Object { $_.href -Match "[0-9]" }
-			$nextUriSubdirectory = $nextURISubdirectoryObject.href | Out-String
-			$nextUriSubdirectory = $nextUriSubdirectory.TrimEnd("`r?`n")
-			$newestVMwareToolsURL = "https://softwareupdate.vmware.com/cds/vmw-desktop/ws/$newestVersion/$nextURISubdirectory/windows/packages/tools-windows.tar"
-			Write-Output "The latest version of VMware tools has been determined to be downloadable from $newestVMwareToolsURL"
-			[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile("$newestVMwareToolsURL", 'C:\Windows\Temp\vmware-tools.tar')
-		}
-		Catch {
-			Write-Output "Unable to determine the latest version of VMware tools. Falling back to hardcoded URL."
-      (New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/ws/15.5.5/16285975/windows/packages/tools-windows.tar', 'C:\Windows\Temp\vmware-tools.tar')
-		}
-		cmd /c "C:\PROGRA~1\7-Zip\7z.exe" x C:\Windows\Temp\vmware-tools.tar -oC:\Windows\Temp
-		Move-Item c:\windows\temp\VMware-tools-windows-*.iso c:\windows\temp\windows.iso
-		Try { Remove-Item "C:\Program Files (x86)\VMWare" -Recurse -Force -ErrorAction Stop } Catch { Write-Output "Directory didn't exist to be removed." }
-	}
-
-	cmd /c "C:\PROGRA~1\7-Zip\7z.exe" x "C:\Windows\Temp\windows.iso" -oC:\Windows\Temp\VMWare
-	cmd /c C:\Windows\Temp\VMWare\setup.exe /S /v"/qn REBOOT=R\"
-
-	Remove-Item -Force "C:\Windows\Temp\vmware-tools.tar"
-	Remove-Item -Force "C:\Windows\Temp\windows.iso"
-	Remove-Item -Force -Recurse "C:\Windows\Temp\VMware"
+<# SECTION Download and install 7-zip #>
+if (!( Test-Path "$env:windir\Temp\7z1900-x64.msi")) {
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	(New-Object System.Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z1900-x64.msi', "$env:windir\Temp\7z1900-x64.msi")
 }
 
-if ("$env:PACKER_BUILDER_TYPE" -eq "virtualbox-iso") {
-	Write-Output "Using Virtualbox"
-	if (Test-Path "C:\Users\vagrant\VBoxGuestAdditions.iso") {
-		Move-Item -Force C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
-	}
-
-	if (!(Test-Path "C:\Windows\Temp\VBoxGuestAdditions.iso")) {
-		Try {
-			$pageContentLinks = (Invoke-WebRequest('https://download.virtualbox.org/virtualbox') -UseBasicParsing).Links | Where-Object { $_.href -Match "[0-9]" } | Select-Object href |  Where-Object { $_.href -NotMatch "BETA" } |  Where-Object { $_.href -NotMatch "RC" } |   Where-Object { $_.href -Match "[0-9]\.[0-9]" } | ForEach-Object { $_.href.Trim('/') }
-			$versionObject = $pageContentLinks | ForEach-Object { New-Object System.Version ($_) } | Sort-Object -Descending | Select-Object -First 1 -Property:Major, Minor, Build
-			$newestVersion = $versionObject.Major.ToString() + "." + $versionObject.Minor.ToString() + "." + $versionObject.Build.ToString() | Out-String
-			$newestVersion = $newestVersion.TrimEnd("`r?`n")
-
-			$nextURISubdirectoryObject = (Invoke-WebRequest("https://download.virtualbox.org/virtualbox/$newestVersion/") -UseBasicParsing).Links | Select-Object href | Where-Object { $_.href -Match "GuestAdditions" }
-			$nextUriSubdirectory = $nextURISubdirectoryObject.href | Out-String
-			$nextUriSubdirectory = $nextUriSubdirectory.TrimEnd("`r?`n")
-			$newestVboxToolsURL = "https://download.virtualbox.org/virtualbox/$newestVersion/$nextUriSubdirectory"
-			Write-Output "The latest version of VirtualBox tools has been determined to be downloadable from $newestVboxToolsURL"
-			[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile("$newestVboxToolsURL", 'C:\Windows\Temp\VBoxGuestAdditions.iso')
-		}
-		Catch {
-			Write-Output "Unable to determine the latest version of VBox tools. Falling back to hardcoded URL."
-			[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object System.Net.WebClient).DownloadFile('https://download.virtualbox.org/virtualbox/6.1.8/VBoxGuestAdditions_6.1.8.iso', 'C:\Windows\Temp\VBoxGuestAdditions.iso')
-		}
-	}
-
-	cmd /c ""C:\PROGRA~1\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
-	Get-ChildItem "C:\Windows\Temp\virtualbox\cert\" -Filter vbox*.cer | ForEach-Object { C:\Windows\Temp\virtualbox\cert\VBoxCertUtil add-trusted-publisher $_.FullName --root $_.FullName }
-	cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
-	cmd /c rd /S /Q "C:\Windows\Temp\virtualbox"
+if (!(Test-Path "$env:windir\Temp\7z1900-x64.msi")) {
+	Start-Sleep 5
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	(New-Object System.Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z1900-x64.msi', "$env:windir\Temp\7z1900-x64.msi")
 }
 
-if ("$env:PACKER_BUILDER_TYPE" -eq "parallels-iso") {
-	Write-Output "Using Parallels Desktop"
-	# This is required when using Parallels Desktop
-	# Currently the installation of Parallels Tools is broken if you're installing in Windows Server & Windows Server Core
-	# You will encounter an error in printui.dll module not found message.
-	# This feature will add the required libraries for printing services.
+msiexec /qb /i "$env:windir\Temp\7z1900-x64.msi"
+<# !SECTION Download and install 7-zip #>
 
-	# Parallels Tools is required for sync_folder to work.
-	# Vagrantfile config: v.update_guest_tools = true will be installed with no issues
-	if ($(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ProductName.StartsWith("Windows Server")) {
-		Write-Output "Installing Print Server service"
-		Install-WindowsFeature -Name Print-Server
-	}  
+<# SECTION Download latest VMwareTools #>
+Try {
+	# Disabling the progress bar speeds up IWR https://github.com/PowerShell/PowerShell/issues/2138
+	$ProgressPreference = 'SilentlyContinue'
+	$pageContentLinks = (Invoke-WebRequest('https://softwareupdate.vmware.com/cds/vmw-desktop/ws') -UseBasicParsing).Links | Where-Object { $_.href -Match "[0-9]" } | Select-Object href | ForEach-Object { $_.href.Trim('/') }
+	$versionObject = $pageContentLinks | ForEach-Object { New-Object System.Version ($_) } | Sort-Object -Descending | Select-Object -First 1 -Property:Major, Minor, Build
+	$newestVersion = $versionObject.Major.ToString() + "." + $versionObject.Minor.ToString() + "." + $versionObject.Build.ToString() | Out-String
+	$newestVersion = $newestVersion.TrimEnd("`r?`n")
+
+	$nextURISubdirectoryObject = (Invoke-WebRequest("https://softwareupdate.vmware.com/cds/vmw-desktop/ws/$newestVersion/") -UseBasicParsing).Links | Where-Object { $_.href -Match "[0-9]" } | Select-Object href | Where-Object { $_.href -Match "[0-9]" }
+	$nextUriSubdirectory = $nextURISubdirectoryObject.href | Out-String
+	$nextUriSubdirectory = $nextUriSubdirectory.TrimEnd("`r?`n")
+	$newestVMwareToolsURL = "https://softwareupdate.vmware.com/cds/vmw-desktop/ws/$newestVersion/$nextURISubdirectory/windows/packages/tools-windows.tar"
+	Write-Output "The latest version of VMware tools has been determined to be downloadable from $newestVMwareToolsURL"
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	(New-Object System.Net.WebClient).DownloadFile("$newestVMwareToolsURL", "$env:windir\Temp\vmware-tools.tar")
+}
+Catch {
+	Write-Output "Unable to determine the latest version of VMware tools. Falling back to hardcoded URL."
+	(New-Object System.Net.WebClient).DownloadFile('https://softwareupdate.vmware.com/cds/vmw-desktop/ws/15.5.5/16285975/windows/packages/tools-windows.tar', "$env:windir\Temp\vmware-tools.tar")
+}
+<# !SECTION Download latest VMwareTools #>
+
+<# SECTION  Unpack and setup VMwareTools install #>
+cmd /c "$env:ProgramFiles\7-Zip\7z.exe" x $env:ProgramFiles\Temp\vmware-tools.tar -o $env:ProgramFiles\Temp
+Move-Item $env:ProgramFiles\temp\VMware-tools-windows-*.iso $env:ProgramFiles\temp\windows.iso
+Try {
+	Remove-Item "C:\Program Files\VMWare" -Recurse -Force -ErrorAction Stop
+}
+Catch {
+	Write-Output "Directory didn't exist to be removed."
 }
 
-cmd /c msiexec /qb /x C:\Windows\Temp\7z1900-x64.msi
+cmd /c "$env:ProgramFiles\7-Zip\7z.exe" x "$env:windir\Temp\windows.iso" -o $env:ProgramFiles\Temp\VMWare
+<# !SECTION  Unpack and setup VMwareTools install #>
+
+# Install VMware Tools
+cmd /c $env:ProgramFiles\Temp\VMWare\setup64.exe /S /v"/qn REBOOT=R\"
+
+<# SECTION Install cleanup #>
+Remove-Item -Force "$env:windir\Temp\vmware-tools.tar"
+Remove-Item -Force "$env:windir\Temp\windows.iso"
+Remove-Item -Force -Recurse "$env:windir\Temp\VMware"
+<# !SECTION Install cleanup #>
+
+cmd /c msiexec /qb /x $env:ProgramFiles\Temp\7z1900-x64.msi
