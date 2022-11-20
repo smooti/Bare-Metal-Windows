c19b00950646061f395a44e654e8b258ee1f844a
smooti <kobeowens43099@gmail.com>
2022-11-20 11:03:10 -0500

    formatting

---
+++ Dsc/Harden-System.ps1
@@ -0,0 +1,74 @@
+Configuration Windows10Stig
+{
+	param
+	(
+		[parameter()]
+		[string]
+		$NodeName = 'localhost'
+	)
+
+	Import-DscResource -ModuleName PowerStig
+	# Internet explorer version information
+	$ieStigVersionObj = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'InternetExplorer' })[-1].Version
+	$ieStigVersion = [string]($ieStigVersionObj).Major + '.' + [string]($ieStigVersionObj).Minor
+	$ieVersion = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'InternetExplorer' })[-1].TechnologyVersion
+
+	# Windows client version information
+	$winClientStigVersionObj = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'WindowsClient' })[-1].Version
+	$winClientStigVersion = [string]($winClientStigVersionObj).Major + '.' + [string]($winClientStigVersionObj).Minor
+	$winClientVersion = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'WindowsClient' })[-1].TechnologyVersion
+
+	# Microsoft edge version information
+	$edgeStigVersionObj = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'MS' })[-1].Version
+	$edgeStigVersion = [string]($edgeStigVersionObj).Major + '.' + [string]($edgeStigVersionObj).Minor
+
+	# Microsoft dotnet version information
+	$dotNetStigVersionObj = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'DotNetFramework' })[-1].Version
+	$dotNetStigVersion = [string]($dotNetStigVersionObj).Major + '.' + [string]($dotNetStigVersionObj).Minor
+	$dotNetVersion = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'DotNetFramework' })[-1].TechnologyVersion
+
+	# Windows Firewall version information
+	$firewallStigVersionObj = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'WindowsFirewall' })[-1].Version
+	$firewallStigVersion = [string]($firewallStigVersionObj).Major + '.' + [string]($firewallStigVersionObj).Minor
+
+	# Microsoft defender version information
+	$defenderStigVersionObj = (Get-Stig -ListAvailable | Where-Object { $_.Technology -eq 'WindowsDefender' })[-1].Version
+	$defenderStigVersion = [string]($defenderStigVersionObj).Major + '.' + [string]($defenderStigVersionObj).Minor
+
+	Node $NodeName
+	{
+		# InternetExplorer InternetExplorerSettings {
+		# 	BrowserVersion = $ieVersion
+		# 	Stigversion    = $ieStigVersion
+		# }
+
+		Edge MSEdge {
+			StigVersion = $edgeStigVersion
+			SkipRule    = @('V-235719')
+		}
+
+		DotNetFramework DotNetFrameworkSettings {
+			FrameworkVersion = $dotNetVersion
+			StigVersion      = $dotNetStigVersion
+		}
+
+		WindowsFirewall FirewallSettings {
+			StigVersion = $firewallStigVersion
+		}
+
+		WindowsDefender DefenderSettings {
+			StigVersion = $defenderStigVersion
+		}
+
+		WindowsClient WindowsSettings {
+			OsVersion   = $winClientVersion
+			Stigversion = $winClientStigVersion
+			SkipRule    = @('V-220704', 'V-220903', 'V-220905', 'V-220906')
+			OrgSettings = '.\Settings\WindowsClient-10.org.default.xml'
+		}
+	}
+}
+
+Windows10Stig
+
+Start-DscConfiguration -Path './' -Wait -Force
--- Scripts/Install-vmWareTools.ps1
+++ Scripts/Install-vmWareTools.ps1
@@ -1,11 +1,11 @@
 # Disable progress bar for fast downloads
 $ProgressPreference = 'SilentlyContinue'
-$vmToolsUrl = "https://packages.vmware.com/tools/esx/latest/windows/x64/"
+$vmToolsUrl = 'https://packages.vmware.com/tools/esx/latest/windows/x64/'
 <# NOTE "-UseBasicParsing" must be used if a machine either does not have IE or has not yet run the initial setup for IE #>
-$vmToolsExeName = ((Invoke-WebRequest -Uri $vmToolsUrl -UseBasicParsing).Links | Where-Object {$_.href -like "VMware-tools-*-x86_64.exe"}).href
+$vmToolsExeName = ((Invoke-WebRequest -Uri $vmToolsUrl -UseBasicParsing).Links | Where-Object { $_.href -like 'VMware-tools-*-x86_64.exe' }).href
 $vmToolsDlUrl = $vmToolsUrl + $vmToolsExeName
 $vmToolslocalFile = "$env:UserProfile\Downloads\$vmToolsExeName"
 
-Invoke-Webrequest -Uri $vmToolsDlUrl -OutFile $vmToolslocalFile -UseBasicParsing
-& $vmToolslocalFile /s /v "/qn REBOOT=ReallySuppress"
+Invoke-WebRequest -Uri $vmToolsDlUrl -OutFile $vmToolslocalFile -UseBasicParsing
+& $vmToolslocalFile /s /v '/qn REBOOT=ReallySuppress'
 # Remove-Item $vmToolslocalFile -Force
