fb75ed54716817c2da42cdd4dbe2e8433967030f
smooti <kobeowens43099@gmail.com>
2023-02-01 07:25:24 -0500

    rewrote to use pkrvars

--- Build-PackerImage.ps1
+++ Build-PackerImage.ps1
@@ -1,52 +1,29 @@
-# NOTE: May add more options for images to build
-param(
-	[Parameter(Mandatory = $false)]
-	[ValidateSet('Win10')]
-	$OsName = 'Win10'
-)
-
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	Write-Warning 'Must be run as admin!'
-	exit
-}
-
 Import-Module '.\Scripts\Modules\vmdk.psm1'
-switch ($OsName) {
-	'Win10' {
-		$osData = @{
-			name      = 'win10'
-			guestType = 'windows9-64'
-			checksum  = 'sha256:D47D3B05C2B4BB42350D569A7B73DA7559EFE461B0E73CD198716195C18799BC'
-			url       = 'Distros/WindowsEnt.iso'
-		}
-	}
-}
-
+$variableFile = '.\vars.pkrvars.hcl'
 $step1Args = @{
 	FilePath     = 'packer.exe'
-	ArgumentList = "build -force -only=step1.vmware-iso.win10-iso -var os_name=$($osData.name) -var iso_checksum=$($osData.checksum) -var iso_url=$($osData.url) -var guest_os_type=$($osData.guestType) ."
+	ArgumentList = "build -force -only=step1.vmware-iso.win10-iso --var-file=$($variableFile) ."
 	wait         = $true
 	NoNewWindow  = $true
 }
 
 $step2Args = @{
 	FilePath     = 'packer.exe'
-	ArgumentList = "build -force -only=step2.vmware-vmx.win10-vmx -var iso_checksum=$($osData.checksum) -var iso_url=$($osData.url) -var os_name=$($osData.name) -var guest_os_type=$($osData.guestType) ."
+	ArgumentList = "build -force -only=step2.vmware-vmx.win10-vmx --var-file=$($variableFile) ."
 	wait         = $true
 	NoNewWindow  = $true
 }
 
 $step3Args = @{
 	FilePath     = 'packer.exe'
-	ArgumentList = "build -force -only=step3.vmware-vmx.win10-vmx -var iso_checksum=$($osData.checksum) -var iso_url=$($osData.url) -var os_name=$($osData.name) -var guest_os_type=$($osData.guestType) ."
+	ArgumentList = "build -force -only=step3.vmware-vmx.win10-vmx --var-file=$($variableFile) ."
 	wait         = $true
 	NoNewWindow  = $true
 }
 
 $step4Args = @{
 	FilePath     = 'packer.exe'
-	ArgumentList = "build -force -only=step4.vmware-vmx.win10-vmx -var iso_checksum=$($osData.checksum) -var iso_url=$($osData.url) -var os_name=$($osData.name) -var guest_os_type=$($osData.guestType) ."
+	ArgumentList = "build -force -only=step4.vmware-vmx.win10-vmx --var-file=$($variableFile) ."
 	wait         = $true
 	NoNewWindow  = $true
 }
